af06af7a291c50d5b10b666c4e70d74bd4ac9f92
devycha <y2kdj9723@naver.com>
2023-07-04 17:58:28 +0900

    Add: Health Check Controller

---
+++ src/main/java/com/fiveam/orderservice/order/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.fiveam.orderservice.order.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+    @GetMapping
+    public ResponseEntity healthCheck() {
+        return ResponseEntity.ok("Health Check Success");
+    }
+}
